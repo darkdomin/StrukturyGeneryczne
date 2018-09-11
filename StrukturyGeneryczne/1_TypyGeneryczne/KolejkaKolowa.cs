cbf175c54136d8be6ebef7d297f056e6f312eaa0
darkdomin <darkdomin@wp.pl>
2018-09-11 20:24:00 +0200

    1_3 KolejkaKolowa

---
+++ StrukturyGeneryczne/1_TypyGeneryczne/KolejkaKolowa.cs
@@ -0,0 +1,57 @@
+﻿namespace _1_TypyGeneryczne
+{
+    public class KolejkaKolowa
+    {
+        private double[] bufor;
+        private int poczatekBufora;
+        private int koniecBufora;
+
+        public KolejkaKolowa() :this (pojemnosc:5)
+        {
+
+        }
+
+        public KolejkaKolowa(int pojemnosc)
+        {
+            bufor = new double[pojemnosc+1];
+            poczatekBufora = 0;
+            koniecBufora = 0;
+        }
+
+        public void Zapisz(double wartosc)
+        {
+            bufor[koniecBufora] = wartosc;
+            //koniec bufora zwieksza sie o 1 ale nie moze byc wiekszy niz jego pojemnosc. Dlatego resta z dzielenia i wielkosc bufora
+            koniecBufora = (koniecBufora + 1) % bufor.Length;
+            if (koniecBufora == poczatekBufora) poczatekBufora = (poczatekBufora + 1)%bufor.Length;
+        }
+        public double Odczyt()
+        {
+            var wynik = bufor[poczatekBufora];
+            poczatekBufora = (poczatekBufora + 1) % bufor.Length;
+            return wynik;
+        }
+        public int Pojemnosc
+        {
+            get
+            {
+                return bufor.Length;
+            }
+        }
+        public bool CzyJestPusty
+        {
+            get
+            {
+                return koniecBufora == poczatekBufora;
+            }
+        }
+        public bool JestPelny
+        {
+            get
+            {
+                return (koniecBufora + 1) % bufor.Length == poczatekBufora;
+            }
+        }
+
+    }
+}
